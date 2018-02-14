124af0b7d09afc49a457443448a3530792fc939b
Ashish Rao <ashishrao2598@gmail.com>
2018-02-14 22:12:18 +0530

    for loop program

---
+++ forLoop.java
@@ -0,0 +1,12 @@
+// for loop program
+
+// The loop will run for the specified conditions in the parameters.
+
+class forLoop {
+    public static void main(String args[]) {
+    int c;
+    for(c = 1; c <= 10; c++){
+        System.out.println(c);
+    }
+}
+}
