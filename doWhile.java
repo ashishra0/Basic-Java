3fbda2507f1062609fde287260c35d0b48545e30
Ashish Rao <ashishrao2598@gmail.com>
2018-02-14 22:11:44 +0530

    do while program

---
+++ doWhile.java
@@ -0,0 +1,14 @@
+// Do while loop program
+
+// do while loop executes a statement until a condition becomes false
+
+class doWhile {
+    public static void main(String args[]){
+    int i = 0;
+    do {
+        System.out.println("i is: " + i);
+        i++;
+    }
+    while(i < 5);
+}
+}
