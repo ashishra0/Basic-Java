e042c0c191fdb00972fdeefefcf812f867243ad5
Ashish Rao <ashishrao2598@gmail.com>
2018-02-14 22:12:34 +0530

    Continue statement

---
+++ continueExample.java
@@ -0,0 +1,18 @@
+// Continue Example
+
+public class continueExample {
+    public static void main(String[] args) {
+        // Continue statement is used to skip a particular iteration of the loop.
+
+        int intArray[] = new int[]{1,2,3,4,5};
+        System.out.println("All numbers except for 3 are: ");
+
+        for (int i=0; i< intArray.length; i++) {
+            if(intArray[i]==3) {
+                continue;
+            } else {
+                System.out.println(intArray[i]);
+            }
+        }
+    }
+}
