8c59c39efd89143e475dcc9cdee85e157e6f8a48
Ashish Rao <ashishrao2598@gmail.com>
2018-02-15 11:25:48 +0530

    first ten odd numbers

---
+++ firstTen.java
@@ -0,0 +1,13 @@
+// Program to print first 10 Odd natural numbers
+
+import java.util.Scanner;
+
+class firstTen {
+    public static void main(String args[]) {
+        int i = 1;
+        while(i<=10){
+            System.out.println(2*i-1);
+            i++;
+        }
+    }
+}
