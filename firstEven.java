39fe04ec307c058031d5863dd166b777f027c1a4
Ashish Rao <ashishrao2598@gmail.com>
2018-02-15 11:26:08 +0530

    first even numbers

---
+++ firstEven.java
@@ -0,0 +1,11 @@
+// program to print first 10 even natural numbers
+
+class firstEven {
+    public static void main(String args[]) {
+        int n = 1;
+        do {
+            System.out.println(2*n);
+            n++;
+        } while (n<=10);
+    }
+}
